304a76253f0f90d998ac6a441c85607bef305372
sayali237t <sayalisthakur7302@gmail.com>
2024-09-25 18:29:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1> Cloud Computing Expt_6 </h1>
-      <h2> Sayali Thakur S8 42493 </h2>
+      <h2> Sayali Thakur </h2>
+      <h3> Batch S8 </h3>
+      <h3> Roll No: 42493 </h3>
     </div>
   );
 }
